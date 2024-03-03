c69bfa8e5b6fad00bffc300dcf6f5848bd9b6ec1
Tim <deasti@icloud.com>
2024-03-03 05:52:28 +0000

    e2e done

--- utilities.js
+++ utilities.js
@@ -1,1 +1,11 @@
-// Add JavaScript below
+// alternative javascript snippet 'hide story' option for future reference:
+// ...
+// function hidestory (el, id) {
+//     for (var i=0; i < 3; i++) { remEl($(id).nextSibling) }
+//     remEl($(id));
+//     fixranks();
+//     var next = (onop() == 'newest' && morenext()) ? ('&next=' + morenext()) : '';
+//     var url = el.href.replace('hide', 'snip-story').replace('goto', 'onop');
+//     fetch(url + next).then(r => r.json()).then(newstory);
+//   }
+//  ...
